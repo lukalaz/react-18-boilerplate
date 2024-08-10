2f0b4c65c1dff607cd6421a0f97e735ab0291bc5
Luka Lazic <luka.web.php@gmail.com>
2024-08-10 18:34:16 +0200

    remove unused css file and clean up App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <h1 className="text-3xl font-bold">Test Tailwind</h1>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-center">Test Tailwind</h1>
+    </div>
+  );
 }
 
 export default App;
