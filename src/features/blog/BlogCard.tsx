79f2caa3e05c4411db8c5baed1e7c399a5719a2a
Luka Lazic <luka.web.php@gmail.com>
2024-08-16 17:17:24 +0200

    Add tailgrids; update tailwind config; create Blog and BlogCard components

---
+++ src/features/blog/BlogCard.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+interface IBlogCard {
+  image: string;
+  date: string;
+  title: string;
+  description: string;
+}
+
+const BlogCard: React.FC<IBlogCard> = ({ image, date, title, description }) => {
+  return (
+    <>
+      <div className="w-full px-4 md:w-1/2 lg:w-1/3">
+        <div className="mb-10 w-full">
+          <div className="mb-8 overflow-hidden rounded">
+            <img src={image} alt="" className="w-full" />
+          </div>
+          <div>
+            {date && (
+              <span className="mb-5 inline-block rounded bg-primary px-4 py-1 text-center text-xs font-semibold leading-loose text-white">
+                {date}
+              </span>
+            )}
+            <h3>
+              <a
+                href="/#"
+                className="mb-4 inline-block text-xl font-semibold text-dark hover:text-primary dark:text-white sm:text-2xl lg:text-xl xl:text-2xl"
+              >
+                {title}
+              </a>
+            </h3>
+            <p className="text-base text-body-color dark:text-dark-6">
+              {description}
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export { BlogCard };
