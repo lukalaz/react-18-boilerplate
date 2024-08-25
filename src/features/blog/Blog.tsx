5d93c1c03f9e531bf41ebd1488bf40fed70b409f
Luka Lazic <luka.web.php@gmail.com>
2024-08-25 17:58:29 +0200

    update text

--- src/features/blog/Blog.tsx
+++ src/features/blog/Blog.tsx
@@ -16,14 +16,20 @@ const Blog = () => {
             <div className="w-full px-4">
               <div className="mx-auto mb-[60px] max-w-[510px] text-center lg:mb-20">
                 <span className="mb-2 block text-lg font-semibold text-primary">
-                  Our Blogs
+                  Our Blog
                 </span>
                 <h2 className="mb-4 text-3xl font-bold text-dark dark:text-white sm:text-4xl md:text-[40px]">
                   Our Recent News
                 </h2>
                 <p className="text-base text-body-color dark:text-dark-6">
-                  There are many variations of passages of Lorem Ipsum available
-                  but the majority have suffered alteration in some form.
+                  The blog posts here are all pulled from{" "}
+                  <a
+                    href="https://newsapi.org/"
+                    target="_blank"
+                    className="text-primary hover:underline"
+                  >
+                    newsapi.org
+                  </a>
                 </p>
               </div>
             </div>
